b1a2f68496158337c617d1d3ed5ad0613bd6c89c
tnreynolds01 <32784506+tnreynolds01@users.noreply.github.com>
2017-11-15 18:50:00 -0600

    Add files via upload

---
+++ Lab 4 - Largest and Smallest Numbers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4___Largest_and_Smallest_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+            int nSmallest;
+            int nLargest;
+            int nCurrent;
+            String strEntry;
+
+            //first integer
+            Console.Write("Enter an integer:");
+            nLargest = nSmallest = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter another integer or Q to Quit");
+            strEntry = Console.ReadLine();
+
+            //loop
+            while (strEntry != "Q") 
+            {
+                nCurrent = Convert.ToInt32(strEntry);
+
+                //check to see if input should replace largest or smallest
+                if (nCurrent > nLargest) 
+                {
+                    nLargest = nCurrent;
+                }
+                else if (nCurrent < nSmallest) 
+                {
+                    nSmallest = nCurrent;
+                }
+
+                Console.Write("Enter another integer or Q to Quit");
+                strEntry = Console.ReadLine();
+            }
+
+            //print results
+            Console.WriteLine("The largest value you entered wass " + nLargest);
+            Console.WriteLine("The smallest value you entered was " + nSmallest);
+
+            Console.ReadLine();
+        }
+
+    }
+}
