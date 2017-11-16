4a0119f6953371d8c9331a069275077149d2787c
tnreynolds01 <32784506+tnreynolds01@users.noreply.github.com>
2017-11-15 18:49:21 -0600

    Add files via upload

---
+++ Lab 3 BMI calculater/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3_BMI_calculater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables 
+            double dblHeight;
+            double dblWeight;
+            double dblBMI;
+
+            //user input
+            Console.Write("EWnter you Height in inches: ");
+
+            //validate loop;
+            while (!double.TryParse(Console.ReadLine(), out dblHeight)) ;
+            {
+                Console.WriteLine("Not valid. Must be an rational number. Enter again: ");
+            }
+            
+
+            Console.Write("EWnter you Wieght in pounds: ");
+            while (!double.TryParse(Console.ReadLine(), out dblWeight))
+            {
+                Console.WriteLine("Not valid. Must be an rational number. Enter again: ");
+            }
+
+            //calculations
+            dblBMI = (dblWeight* 703) / (dblHeight * dblHeight);
+
+            //display result
+            Console.WriteLine("Your BMI is " + dblBMI);
+
+            //display message 
+            if (dblBMI <= 18.5) 
+            {
+                Console.WriteLine("BMI < 18.5 Underweight");
+            }
+            else if (dblBMI <= 24.9)
+            {
+                Console.WriteLine("BMI between 18.5 and 24.9 - Normal");
+            }
+            else if (dblBMI <= 29.9)
+            {
+                Console.WriteLine("BMI between 25 and 29.9 - OverWieght");
+            }
+            else
+            {
+                Console.WriteLine("BMI >= 30 - Obese");
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
