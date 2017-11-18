bb2bf737431583d86448cacf957abdf709a9a92d
tnreynolds01 <32784506+tnreynolds01@users.noreply.github.com>
2017-11-17 22:36:00 -0600

    Add files via upload

---
+++ HW1 - Variables and Arithmetic/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1___Variables_and_Arithmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//variable declaration
+            
+            int nNum1, nNum2;
+            double dblAnswer;
+
+            //prompt for num1, and num2
+            Console.WriteLine("Enter the a number :");
+            nNum1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a number :");
+            nNum2 = Convert.ToInt32(Console.ReadLine());
+
+            //calculation
+            dblAnswer = (double)nNum1 / nNum2;
+            Console.WriteLine(nNum1+" / "+nNum2+" = "+dblAnswer);
+            dblAnswer = nNum1 + nNum2;
+            Console.WriteLine(nNum1 + " + " + nNum2 + " = " + dblAnswer);
+            dblAnswer = nNum1 - nNum2;
+            Console.WriteLine(nNum1 + " - " + nNum2 + " = " + dblAnswer);
+            dblAnswer = nNum1 * nNum2;
+            Console.WriteLine(nNum1 + " * " + nNum2 + " = " + dblAnswer);
+
+            Console.ReadLine();
+          
+        }
+    }
+}
