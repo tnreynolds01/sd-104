a01875e7da541545e426d05f19eb6bb024792354
tnreynolds01 <32784506+tnreynolds01@users.noreply.github.com>
2017-11-22 19:44:42 -0600

    Add files via upload

---
+++ HW 2- Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_2__Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secondPrev = 0;
+            int prev = 1;
+            int current = 0;
+            int userInput;
+
+            Console.WriteLine("Start with 1 and 1 as pervious two numbers and add");
+            userInput = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < userInput; i++)
+            {
+                current = prev + secondPrev;
+                secondPrev = prev;
+                prev = current;                
+            }
+            Console.WriteLine(current);
+            Console.ReadLine();
+        }
+    }
+}
